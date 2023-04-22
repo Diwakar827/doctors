e7fcee03ea03192606c45e017b810803b1a49684
Diwakar <diwakarchauhaun827@gmail.com>
2023-04-22 11:24:18 +0530

    login functionality

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,29 +1,56 @@
-import React from 'react';
+import React from "react";
 import { Button, Form, Input } from "antd";
-import { Link} from "react-router-dom";
-import '../styles/Logandregister.css';
-
+import { Link, useNavigate } from "react-router-dom";
+import "../styles/Logandregister.css";
+import axios from "axios";
+import toast from "react-hot-toast";
 const Login = () => {
-    return (
-        
-        <div className="authentication">
-        <div className="authenticationForm card p-2" >
-          <h1 className="card-title">Welcome to the Health Care</h1>
-  
-          <Form layout="vertical" >
-         
-             <Form.Item label="Email" name="email">
-              <Input placeholder="email" />
-            </Form.Item>
-            <Form.Item label="Password" name="password">
-              <Input placeholder="password" />
-            </Form.Item>
-            <Button type="primary" htmlType="submit">Login</Button>
-          </Form>
-          <Link to='/register' className="anchor mt-2" >FirstTime?Click for Register</Link>
-        </div>
+  const navigate = useNavigate();
+
+  const onFinish = async (values) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/userchecking/login",
+        values
+      );
+
+      console.log(response);
+
+      if (response.data.success) {
+        console.log(response.data.token);
+        toast.success(response.data.message);
+        toast.success("navigating to home page");
+        navigate("/");
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (err) {
+      toast.error("something went wrong");
+    }
+  };
+
+  return (
+    <div className="authentication">
+      <div className="authenticationForm card p-2">
+        <h1 className="card-title">Welcome to the Health Care</h1>
+
+        <Form layout="vertical" onFinish={onFinish}>
+          <Form.Item label="Email" name="email">
+            <Input placeholder="email" />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input placeholder="password" />
+          </Form.Item>
+          <Button type="primary" htmlType="submit">
+            Login
+          </Button>
+        </Form>
+        <Link to="/register" className="anchor mt-2">
+          FirstTime?Click for Register
+        </Link>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Login;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,46 +1,75 @@
 const express = require("express");
 
 const bcrypt = require("bcrypt");
-
+const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 const router = express.Router();
 
-router.post("/register", async (req, res,next) => {
-  try{
-    
+router.post("/register", async (req, res, next) => {
+  try {
     console.log("request came");
-    const name=req.body.name;
-  const email = req.body.email;
-  const password = req.body.password;
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
 
-  const userexists = await userModel.findOne({email:email});
+    const userexists = await userModel.findOne({ email: email });
 
-  if (userexists)
-   {
-    console.log("user exists");
-    return  res.status(200).json({ message: "user already exists", success: false });
-  } 
-  else 
-  {
-     
-    const salt = await bcrypt.genSalt(10);
-     const hashedPassword = await bcrypt.hash(password, salt);
-    const newUser = new userModel({
-      name: name,
-      email: email,
-      password: hashedPassword,
-    });
-    await newUser.save();
-    return res.status(200).json({ message: "user successfully registered", success: true });
-   }
-}
-    catch(err)
-    {
-        console.log(err);
-       return res.status(400).json({ message: "something went wrong",error:err, success: false});
+    if (userexists) {
+      console.log("user exists");
+      return res
+        .status(200)
+        .json({ message: "user already exists", success: false });
+    } else {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      const newUser = new userModel({
+        name: name,
+        email: email,
+        password: hashedPassword,
+      });
+      await newUser.save();
+      return res
+        .status(200)
+        .json({ message: "user successfully registered", success: true });
     }
-    next();
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(400)
+      .json({ message: "something went wrong", error: err, success: false });
+  }
+  next();
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    const user = await userModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res
+        .status(200)
+        .send({ message: "user doesnot exists", success: false });
+    }
 
-module.exports=router;
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+
+    if (!isMatch) {
+      return res
+        .status(200)
+        .send({ message: "password is incorrect", success: false });
+    } else {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+      });
+      return res
+        .status(200)
+        .send({ message: "Login Succesful", success: true, data: token });
+    }
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .send({ message: "Error Logging in", success: false, data: error });
+  }
+});
+
+module.exports = router;
