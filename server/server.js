b9da5215e693f0976c8224d4915f1d0886ea9e2e
Diwakar <diwakarchauhaun827@gmail.com>
2023-08-20 04:30:38 +0530

    not working

--- server/server.js
+++ server/server.js
@@ -10,17 +10,20 @@ const doctorRouter=require("./routes/doctorRoutes");
 const cors=require('cors');
 
 
+/*
 app.use(cors({
   origin:["*"],
   methods: ["GET", "POST", "PUT", "DELETE"], 
   credentials:true
 }))
+*/
 
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("credentials","true");
   next();
 })
 
