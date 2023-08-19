2b22217d64e73a063eefa04a17b16cad28db40cb
Diwakar <diwakarchauhaun827@gmail.com>
2023-08-20 04:12:30 +0530

    not figured

--- server/server.js
+++ server/server.js
@@ -17,6 +17,13 @@ app.use(cors({
 }))
 */
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
+
 app.use(cors());
 
 app.use(express.json());
